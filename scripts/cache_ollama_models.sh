af88b2958f74b7599d6d8acd1658b05394838594
Filomilo <fborowiec@wp.pl>
2025-08-07 14:26:11 +0200

    decreaea mount of modles

--- scripts/cache_ollama_models.sh
+++ scripts/cache_ollama_models.sh
@@ -3,20 +3,20 @@
 docker exec ollama ollama pull gemma3:1b
 docker exec ollama ollama pull gemma3
 docker exec ollama ollama pull gemma3:12b
-docker exec ollama ollama pull deepseek-r1
-docker exec ollama ollama pull llama3.2
-docker exec ollama ollama pull llama3.2:1b
-docker exec ollama ollama pull llama3.1
-docker exec ollama ollama pull phi4
-docker exec ollama ollama pull phi4-mini
-docker exec ollama ollama pull mistral
-docker exec ollama ollama pull moondream
-docker exec ollama ollama pull neural-chat
-docker exec ollama ollama pull starling-lm
-docker exec ollama ollama pull codellama
-docker exec ollama ollama pull llama2-uncensored
-docker exec ollama ollama pull llava
-docker exec ollama ollama pull granite3.3
+# docker exec ollama ollama pull deepseek-r1
+# docker exec ollama ollama pull llama3.2
+# docker exec ollama ollama pull llama3.2:1b
+# docker exec ollama ollama pull llama3.1
+# docker exec ollama ollama pull phi4
+# docker exec ollama ollama pull phi4-mini
+# docker exec ollama ollama pull mistral
+# docker exec ollama ollama pull moondream
+# docker exec ollama ollama pull neural-chat
+# docker exec ollama ollama pull starling-lm
+# docker exec ollama ollama pull codellama
+# docker exec ollama ollama pull llama2-uncensored
+# docker exec ollama ollama pull llava
+# docker exec ollama ollama pull granite3.3
 
 docker stop ollama
 docker rm ollama
