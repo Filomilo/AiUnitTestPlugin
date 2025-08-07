c6cc7ab82a658edbfe10becde794be2f883e4139
Filomilo <fborowiec@wp.pl>
2025-08-07 15:53:22 +0200

    removed model and removed build tes

--- scripts/cache_ollama_models.sh
+++ scripts/cache_ollama_models.sh
@@ -3,7 +3,7 @@
 docker exec ollama ollama pull gemma3:1b
 docker exec ollama ollama pull gemma3
 docker exec ollama ollama pull gemma3:12b
-docker exec ollama ollama pull deepseek-r1
+# docker exec ollama ollama pull deepseek-r1
 # docker exec ollama ollama pull llama3.2
 # docker exec ollama ollama pull llama3.2:1b
 # docker exec ollama ollama pull llama3.1
