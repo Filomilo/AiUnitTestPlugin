67ece35a983c93a65f19e619176970eb26ec78ed
Filomilo <fborowiec@wp.pl>
2025-08-07 14:31:51 +0200

    incras amotn of models

--- scripts/cache_ollama_models.sh
+++ scripts/cache_ollama_models.sh
@@ -3,9 +3,9 @@
 docker exec ollama ollama pull gemma3:1b
 docker exec ollama ollama pull gemma3
 docker exec ollama ollama pull gemma3:12b
-# docker exec ollama ollama pull deepseek-r1
-# docker exec ollama ollama pull llama3.2
-# docker exec ollama ollama pull llama3.2:1b
+docker exec ollama ollama pull deepseek-r1
+docker exec ollama ollama pull llama3.2
+docker exec ollama ollama pull llama3.2:1b
 # docker exec ollama ollama pull llama3.1
 # docker exec ollama ollama pull phi4
 # docker exec ollama ollama pull phi4-mini
