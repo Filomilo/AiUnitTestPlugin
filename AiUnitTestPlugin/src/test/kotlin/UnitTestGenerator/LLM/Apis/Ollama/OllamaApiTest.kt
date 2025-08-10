355766b4f02006b772e37ad57e1c57c07a6cd0af
Filomilo <fborowiec@wp.pl>
2025-08-10 10:55:31 +0200

    added dynacmic creation of coantienrs

--- AiUnitTestPlugin/src/test/kotlin/UnitTestGenerator/LLM/Apis/Ollama/OllamaApiTest.kt
+++ AiUnitTestPlugin/src/test/kotlin/UnitTestGenerator/LLM/Apis/Ollama/OllamaApiTest.kt
@@ -20,7 +20,7 @@ import kotlin.random.Random
 
 //@Disabled("temporaryl")
 class OllamaApiTest {
-    //    var OllamaContainer: OllamaContainer? = null
+    var OllamaContainer: OllamaContainer? = null
     var ollamaPort: Int = 2325;
     var OllamaApi: OllamaApi? = null
     val model = "gemma3:1b"
@@ -30,10 +30,10 @@ class OllamaApiTest {
     fun setUp() {
 
         this.ollamaPort = Random.nextInt(10000, 20000)
-//        this.OllamaContainer = OllamaContainer(
-//            DockerConnection, port = ollamaPort, ramBytes = 1024L * 1024L * 1024L * 2L
-//        )
-//        this.OllamaContainer!!.start();
+        this.OllamaContainer = OllamaContainer(
+            DockerConnection, port = ollamaPort, ramBytes = 1024L * 1024L * 1024L * 2L
+        )
+        this.OllamaContainer!!.start();
         this.OllamaApi = OllamaApi(
             "http://ollama:11434/"
         )
@@ -57,7 +57,7 @@ class OllamaApiTest {
 
     @AfterEach
     fun tearDown() {
-//        this.OllamaContainer!!.destroy()
+        this.OllamaContainer!!.destroy()
     }
 
     //    @Disabled("temporaryl")
