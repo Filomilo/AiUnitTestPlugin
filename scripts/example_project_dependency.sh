e4604b53912113615a62b16ac3b5f30eb7c80cb1
Filomilo <fborowiec@wp.pl>
2025-08-24 17:49:00 +0200

    remove sudo from depdnecy installer

--- scripts/example_project_dependency.sh
+++ scripts/example_project_dependency.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-sudo apt update
-sudo apt install -y python3 python3-pip
+apt update
+apt install -y python3 python3-pip
 python3 --version
 pip3 --version
 pip3 install pytest
 pytest --version
 
 
-sudo apt install -y maven
+apt install -y maven
 mvn -version
