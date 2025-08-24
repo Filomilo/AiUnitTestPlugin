ff63c4aa2f2e6ce71caf971aa9f5340b23c50bf3
Filomilo <fborowiec@wp.pl>
2025-08-24 12:26:17 +0200

    added dso 2 unix on all scirpt

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,2 +1,12 @@
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+for f in "$SCRIPT_DIR"/*.sh; do
+        dos2unix "$f"
+        chmod +x "$f"
+done 
+
+
+
 "$SCRIPT_DIR"/UpdateLexers.sh
