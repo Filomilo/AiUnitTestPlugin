ab766ab886c020a4a0af7c0a2458acccd85eeccc
Filomilo <60512955+Filomilo@users.noreply.github.com>
2025-08-24 12:36:21 +0200

    21 fix buildiing script in gh action (#22)
    * add tos acceptanxw
    
    * another way tos accpetence
    
    * removed jar dependecy
    
    * add prebuidl run job
    
    * preserp prebuidl script for run
    
    * run prebuild sripts by itself
    
    * removed bash decalration
    
    * added dso 2 unix on all scirpt
    
    * addd bask decarlation

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+for f in "$SCRIPT_DIR"/*.sh; do
+        dos2unix "$f"
+        chmod +x "$f"
+done 
+
+
+
 "$SCRIPT_DIR"/UpdateLexers.sh
