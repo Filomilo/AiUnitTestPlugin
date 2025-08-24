48812831a7a7f8178dcc352e47cae49a6d8bd289
Filomilo <fborowiec@wp.pl>
2025-08-24 11:45:45 +0200

    another way tos accpetence

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,16 @@ rootProject.name = "AiUnitTestPlugin"
 
 include(":AiTestGenerator")
 include(":AiTestGenerotorAnalisis")
+plugins {
+    id("com.gradle.develocity") version "3.17.5"
+}
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+        termsOfUseAgree = "yes"
+
+        // publishing.onlyIf { false }
+        // If you don't need to auto-publish the build scan, uncomment it.
+
+    }
+}
