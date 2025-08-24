bbaf5d2924b40be78816b0d11b59f573fecae61f
Filomilo <fborowiec@wp.pl>
2025-08-24 10:22:23 +0200

    created prebuild script

--- scripts/UpdateLexers.sh
+++ scripts/UpdateLexers.sh
@@ -1,15 +1,13 @@
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 curl -o JavaLexer.g4 https://raw.githubusercontent.com/antlr/grammars-v4/refs/heads/master/java/java/JavaLexer.g4
 curl -o JavaParser.g4 https://raw.githubusercontent.com/antlr/grammars-v4/refs/heads/master/java/java/JavaParser.g4
 curl -o JavaParserBase.java https://raw.githubusercontent.com/antlr/grammars-v4/refs/heads/master/java/java/Java/JavaParserBase.java
 
-java -jar antlr-4.13.2-complete.jar -listener   -visitor  -Dlanguage=Java JavaLexer.g4
-java -jar antlr-4.13.2-complete.jar -listener    -Dlanguage=Java JavaParser.g4
+mkdir -p "$SCRIPT_DIR/../AiTestGenerator/src/main/java/Tools/CodeParsers/ANTLR/"
 
 
-mkdir -p ../AiTestGenerator/src/main/java/Tools/CodeParsers/ANTLR/
+java -jar "$SCRIPT_DIR"/antlr-4.13.2-complete.jar  -package Tools.CodeParsers.ANTLR -listener   -visitor  -Dlanguage=Java JavaLexer.g4 -o "$SCRIPT_DIR/../AiTestGenerator/src/main/java/Tools/CodeParsers/ANTLR/" 
+java -jar "$SCRIPT_DIR"/antlr-4.13.2-complete.jar  -package Tools.CodeParsers.ANTLR -listener    -Dlanguage=Java JavaParser.g4 -o "$SCRIPT_DIR/../AiTestGenerator/src/main/java/Tools/CodeParsers/ANTLR/" 
 
 
-for f in *.java; do
-    echo "package Tools.CodeParsers.ANTLR;" | cat - "$f" > temp && mv temp "$f"
-    mv "$f" ../AiTestGenerator/src/main/java/Tools/CodeParsers/ANTLR/"$f"
-done
