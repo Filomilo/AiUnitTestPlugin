6f090ffed16389b9f052ba5ae993db1d18f930ae
Filomilo <fborowiec@wp.pl>
2025-08-26 13:25:11 +0200

    added base interface

---
+++ AiTestGenerotorAnalisis/src/main/kotlin/CodeMetric/MultiMetricCodeMetricCalculator.kt
@@ -0,0 +1,9 @@
+package org.filomilo.AiTestGenerotorAnalisis.CodeMetric
+
+import java.nio.file.Path
+
+class MultiMetricCodeMetricCalculator: CodeMetricCalculator {
+    override fun calculateCodeMetricsForDirectory(path: Path): CodeMetricReport {
+        TODO("Not yet implemented")
+    }
+}
