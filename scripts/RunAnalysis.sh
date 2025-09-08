7114ae6f6f99322cbbb7f655c3965cb109532151
Filomilo <fborowiec@wp.pl>
2025-09-08 21:19:18 +0200

    add ram limit

--- scripts/RunAnalysis.sh
+++ scripts/RunAnalysis.sh
@@ -2,4 +2,6 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 chmod +x  "$SCRIPT_DIR"/../AiTestGenerotorAnalisis/gradlew
 echo "------------------run dradlew"
- "$SCRIPT_DIR"/../AiTestGenerotorAnalisis/gradlew run -x test --scan --no-daemon -Dorg.gradle.jvmargs=-XX:+UseContainerSupport --info --build-cache
+"$SCRIPT_DIR"/../AiTestGenerotorAnalisis/gradlew run -x test --scan --no-daemon \
+  -Dorg.gradle.jvmargs="-Xmx1g -XX:+UseContainerSupport" \
+  --info --build-cache
