006607cd5b3f1feec6df071c3a8e64e896b76af5
Filomilo <fborowiec@wp.pl>
2025-09-08 20:39:42 +0200

    added additon mmry info

--- scripts/RunAnalysis.sh
+++ scripts/RunAnalysis.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 chmod +x  "$SCRIPT_DIR"/../AiTestGenerotorAnalisis/gradlew
+echo "------------------run dradlew"
  "$SCRIPT_DIR"/../AiTestGenerotorAnalisis/gradlew run -x test --scan --no-daemon -Dorg.gradle.jvmargs=-XX:+UseContainerSupport --info --build-cache
