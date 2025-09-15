f1e3104001c88836308db118c179692109c29b6a
Filomilo <fborowiec@wp.pl>
2025-09-15 23:43:26 +0200

    removed sudo

--- scripts/BuildWebpage.sh
+++ scripts/BuildWebpage.sh
@@ -1,7 +1,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-sudo apt-get update
-sudo apt-get install -y curl
+apt-get update
+apt-get install -y curl
 curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
-sudo apt-get install -y nodejs
+apt-get install -y nodejs
 yarn --cwd "$SCRIPT_DIR"/../AnalysisResultWebpage/ install
 yarn --cwd "$SCRIPT_DIR"/../AnalysisResultWebpage/ build
