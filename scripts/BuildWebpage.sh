b1beef75655db86351a7a03770a0b021e06b5500
Filomilo <fborowiec@wp.pl>
2025-09-15 23:48:50 +0200

    added np intall yarn

--- scripts/BuildWebpage.sh
+++ scripts/BuildWebpage.sh
@@ -3,5 +3,7 @@ apt-get update
 apt-get install -y curl
 curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
 apt-get install -y nodejs
+npm install -g yarn
+
 yarn --cwd "$SCRIPT_DIR"/../AnalysisResultWebpage/ install
 yarn --cwd "$SCRIPT_DIR"/../AnalysisResultWebpage/ build
