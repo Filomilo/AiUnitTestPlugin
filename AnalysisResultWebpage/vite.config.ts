481f30bf158ed26ae8e45e14a16eb33b41c94d5c
Filomilo <fborowiec@wp.pl>
2025-09-16 07:59:03 +0200

    added base

--- AnalysisResultWebpage/vite.config.ts
+++ AnalysisResultWebpage/vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/AiUnitTestPlugin/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue', '.cache'],
