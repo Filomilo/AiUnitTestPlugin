3fa278ebf832d8314504840b791894a1c24c3cc2
Filomilo <fborowiec@wp.pl>
2025-09-16 06:24:41 +0200

    added nvm yarn instakatiokon

--- scripts/BuildWebpage.sh
+++ scripts/BuildWebpage.sh
@@ -1,10 +1,30 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 apt-get update
 apt-get install -y curl
-curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
-apt-get install -y nodejs
-apt-get install npm -y
-echo "instlal yarn"
-npm install -g yarn
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.19.0".
+
+# Download and install Yarn:
+corepack enable yarn
+
+# Verify Yarn version:
+yarn -v
+
+
+
+
+
 yarn --cwd "$SCRIPT_DIR"/../AnalysisResultWebpage/ install
 yarn --cwd "$SCRIPT_DIR"/../AnalysisResultWebpage/ build
