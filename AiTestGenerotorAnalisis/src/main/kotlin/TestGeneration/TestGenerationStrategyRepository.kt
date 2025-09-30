cf2f8324599363dbfa7dc01b6c84ab0f8766a745
Filomilo <fborowiec@wp.pl>
2025-09-30 20:54:42 +0200

    borught back llms and strategies

--- AiTestGenerotorAnalisis/src/main/kotlin/TestGeneration/TestGenerationStrategyRepository.kt
+++ AiTestGenerotorAnalisis/src/main/kotlin/TestGeneration/TestGenerationStrategyRepository.kt
@@ -8,71 +8,71 @@ import org.filomilo.AiTestGenerotorAnalisis.TestGeneration.Strategy.TestGenerati
 
 object TestGenerationStrategyRepository {
     val strategies: Collection<TestGenerationStrategy> = mutableListOf(
-//        ManualTestsStrategy(),
-//        PromptPerMethodStrategy(
-//            """
-//                Create ##framework## tests for function ##functions##
-//            """.trimIndent(), listOf(
-//                TestGenerationStrategy.Tags.PER_METHOD
-//            )
-//        ),
-//        PromptPerMethodStrategy(
-//            """
-//                Create ##framework## tests for function
-//
-//                ##functions##
-//
-//                with project structure
-//
-//                ##tree##
-//            """.trimIndent(), listOf(
-//                TestGenerationStrategy.Tags.PER_METHOD, TestGenerationStrategy.Tags.WITH_PROJECT_TREE
-//            )
-//        ),
-//        PromptPerFileStrategy(
-//            """
-//                Create ##framework## tests for file ##filename## ::
-//                 ##filecontent##
-//            """.trimIndent(), listOf(
-//                TestGenerationStrategy.Tags.PER_FILE
-//            )
-//        ),
-//        PromptPerFileStrategy(
-//            """
-//                Create ##framework## tests for file ##filename## ::
-//                 ##filecontent##
-//
-//                  with project structure
-//                  ##tree##
-//            """.trimIndent(), listOf(
-//                TestGenerationStrategy.Tags.PER_FILE, TestGenerationStrategy.Tags.WITH_PROJECT_TREE
-//            )
-//        ),
-//        PromptPerFileStrategy(
-//            """
-//                Create ##framework## tests for file ##filename## ::
-//                 ##filecontent##
-//
-//                  with project structure
-//                  ##tree##
-//
-//                  do not redefine code but import
-//            """.trimIndent(), listOf(
-//                TestGenerationStrategy.Tags.PER_FILE,
-//                TestGenerationStrategy.Tags.WITH_PROJECT_TREE,
-//                TestGenerationStrategy.Tags.IMPORT_SPECIFICATION
-//            )
-//        ),
-//        PromptPerFileStrategy(
-//            """
-//                Create ##framework## tests for file ##filename## ::
-//                 ##filecontent##
-//
-//                  do not redefine code but import
-//            """.trimIndent(), listOf(
-//                TestGenerationStrategy.Tags.PER_FILE, TestGenerationStrategy.Tags.IMPORT_SPECIFICATION
-//            )
-//        ),
+        ManualTestsStrategy(),
+        PromptPerMethodStrategy(
+            """
+                Create ##framework## tests for function ##functions##
+            """.trimIndent(), listOf(
+                TestGenerationStrategy.Tags.PER_METHOD
+            )
+        ),
+        PromptPerMethodStrategy(
+            """
+                Create ##framework## tests for function
+
+                ##functions##
+
+                with project structure
+
+                ##tree##
+            """.trimIndent(), listOf(
+                TestGenerationStrategy.Tags.PER_METHOD, TestGenerationStrategy.Tags.WITH_PROJECT_TREE
+            )
+        ),
+        PromptPerFileStrategy(
+            """
+                Create ##framework## tests for file ##filename## ::
+                 ##filecontent##
+            """.trimIndent(), listOf(
+                TestGenerationStrategy.Tags.PER_FILE
+            )
+        ),
+        PromptPerFileStrategy(
+            """
+                Create ##framework## tests for file ##filename## ::
+                 ##filecontent##
+
+                  with project structure
+                  ##tree##
+            """.trimIndent(), listOf(
+                TestGenerationStrategy.Tags.PER_FILE, TestGenerationStrategy.Tags.WITH_PROJECT_TREE
+            )
+        ),
+        PromptPerFileStrategy(
+            """
+                Create ##framework## tests for file ##filename## ::
+                 ##filecontent##
+
+                  with project structure
+                  ##tree##
+
+                  do not redefine code but import
+            """.trimIndent(), listOf(
+                TestGenerationStrategy.Tags.PER_FILE,
+                TestGenerationStrategy.Tags.WITH_PROJECT_TREE,
+                TestGenerationStrategy.Tags.IMPORT_SPECIFICATION
+            )
+        ),
+        PromptPerFileStrategy(
+            """
+                Create ##framework## tests for file ##filename## ::
+                 ##filecontent##
+
+                  do not redefine code but import
+            """.trimIndent(), listOf(
+                TestGenerationStrategy.Tags.PER_FILE, TestGenerationStrategy.Tags.IMPORT_SPECIFICATION
+            )
+        ),
 
         JsonResponsePerFileStrategy(
             """
